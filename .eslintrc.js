f713c95f064ca5187448e5348abc67ed7d8e9ae8
Marc Vilella <mvilella@obsa.ad>
2019-06-12 19:45:04 +0200

    Reset CLI default config
    + Add custom eslint rules
    + Delete default pages and components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-tabs': 'off',
+		'indent': ['error', 'tab', {
+			'SwitchCase': 1
+		}],
+		'max-len': ['error', {
+			'code': 100,
+			'ignoreTrailingComments': true,
+			'ignoreUrls': true,
+			'ignoreStrings': true
+		}],
+		'no-param-reassign': ['error', {
+			'props': true,
+			'ignorePropertyModificationsFor': [
+				'acc', // for reduce accumulators
+				'state', // for Vuex store
+			]
+		}],
+		'no-shadow': ['error', { 'allow': ['response', 'data'] }],
+		'object-curly-newline': ['error', { 'multiline': true }],
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 };
