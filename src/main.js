bccdbdf700b369a32e40308b5baa3bc010a258aa
Marc Vilella <mvilella@obsa.ad>
2019-06-12 20:20:14 +0200

    Install Eric Meyer's reset.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import 'reset-css'; // eslint-disable-line import/extensions
 import '@mdi/font/css/materialdesignicons.css';
 import App from './App.vue';
 import router from './router';
