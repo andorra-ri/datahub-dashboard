2508281bdaa397771e22919c6977bc5c97457e5c
Marc Vilella <mvilella@obsa.ad>
2019-06-13 19:26:18 +0200

    Add [hidden] UI page

---
+++ src/views/UserInterface.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+	<div class="screen">
+		<main class="content">
+			<header class="header">
+				<h1><em>SETTINGS</em>USER INTERFACE</h1>
+			</header>
+		</main>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'tourism',
+};
+</script>
