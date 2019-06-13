122c99beeeabe5453be8639414ac12603ce5407b
Marc Vilella <mvilella@obsa.ad>
2019-06-13 19:23:12 +0200

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
 	<div id="app">
+		<nav-bar />
+		<router-view />
 	</div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+	name: 'innohub',
+	components: { NavBar },
+};
+</script>
 
 <style lang="scss">
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+	<div class="navbar">
+		<router-link to="/" class="brand">
+			<img src="../assets/img/logo-actuatech.png" />
+		</router-link>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+	align-items: center;
+	background: #fff;
+	border-right: 1px solid #dadde1;
+	display: inline-flex;
+	flex-direction: column;
+	height: 100vh;
+	justify-content: center;
+
+	&__anchor {
+		color: #bac4d2;
+		display: block;
+		font-size: 20px;
+		margin: 10px 0;
+
+		&:hover,
+		&.router-link-exact-active { color: #461ea3; }
+	}
+
+	.brand {
+		display: block;
+		margin-bottom: auto;
+
+		img {
+			height: 30px;
+			margin: 15px;
+		}
+	}
+
+	.user {
+		margin-top: auto;
+
+		.avatar {
+			border-radius: 50%;
+			margin: 10px 0;
+			width: 30px;
+		}
+	}
+}
+</style>
