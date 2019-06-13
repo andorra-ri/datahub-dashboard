777972dcb9822b41f78d55cc03974690e130b916
Marc Vilella <mvilella@obsa.ad>
2019-06-13 19:30:29 +0200

    Add grid system

--- src/views/UserInterface.vue
+++ src/views/UserInterface.vue
@@ -4,6 +4,22 @@
 			<header class="header">
 				<h1><em>SETTINGS</em>USER INTERFACE</h1>
 			</header>
+			<div class="grid">
+				<div class="row">
+					<div class="column">
+						<section class="card card--round">
+							<h2>Buttons</h2>
+						</section>
+					</div>
+				</div>
+				<div class="row">
+					<div class="column">
+						<section class="card card--round">
+							<h2>Forms</h2>
+						</section>
+					</div>
+				</div>
+			</div>
 		</main>
 	</div>
 </template>
