338de6e7ac2a100d6f2d92500260e72b0eff69c4
Marc Vilella <mvilella@obsa.ad>
2019-06-13 19:31:48 +0200

    Add buttons style

--- src/views/UserInterface.vue
+++ src/views/UserInterface.vue
@@ -9,6 +9,26 @@
 					<div class="column">
 						<section class="card card--round">
 							<h2>Buttons</h2>
+							<button>Settings</button>
+							<button><i class="icon mdi mdi-vuejs" /> Settings</button>
+							<button><i class="icon mdi mdi-settings" /></button>
+							<button class="outline">
+								<i class="icon mdi mdi-setting" />
+								Settings
+							</button>
+							<button disabled><i class="icon mdi mdi-settings" /> Settings</button>
+							<fieldset class="group">
+								<button><i class="icon mdi mdi-vuejs" /></button>
+								<button><i class="icon mdi mdi-angular" /></button>
+								<button><i class="icon mdi mdi-react" /></button>
+								<button><i class="icon mdi mdi-codepen" /></button>
+							</fieldset>
+							<fieldset class="group">
+								<button class="outline"><i class="icon mdi mdi-vuejs" /></button>
+								<button class="outline"><i class="icon mdi mdi-angular" /></button>
+								<button class="outline"><i class="icon mdi mdi-react" /></button>
+								<button class="outline"><i class="icon mdi mdi-codepen" /></button>
+							</fieldset>
 						</section>
 					</div>
 				</div>
